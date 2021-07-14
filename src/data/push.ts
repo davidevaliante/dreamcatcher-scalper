9dfc751dbe137046c4bf3cbc82cac16191083df7
davidevaliante <dav.valiante@gmail.com>
2021-07-14 15:32:29 +0200

    changed api with readable adress

--- src/data/push.ts
+++ src/data/push.ts
@@ -2,9 +2,9 @@ import axios from 'axios'
 import { Spin } from '../models/Spin'
 import { Symbol } from '../models/Symbol'
 
-// const API_ROOT = 'https://dreamcatcher.spike-realtime-api.eu/api'
+const API_ROOT = 'https://dreamcatcher.spike-realtime-api.eu/api'
 
-const API_ROOT = 'http://localhost:5004/api'
+// const API_ROOT = 'http://localhost:5004/api'
 
 
 export const pushRows = async (rows : Spin[]) => {
